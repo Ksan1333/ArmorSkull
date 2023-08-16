7a7a6137c9892fd409736a04ca0adf17c6d9a57c
Ksan1333 <Ninjin270103@outlook.jp>
2023-08-16 10:43:39 +0900

    First Commit

---
+++ src/main/java/com/keisa1333/armorskull/ArmorSkull.java
@@ -0,0 +1,18 @@
+package com.keisa1333.armorskull;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorSkull extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hello World!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
